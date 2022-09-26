a639d2c9abd83145d31ad05cdc01a362da8f0076
Anatoliy K <778966@mail.ru>
2022-09-26 23:40:20 +0500

    added 28

---
+++ Task_28/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача:
+// Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N.
+// Например:
+// 4 -> 24
+// 5 -> 120
+
+int ReadData(string msg) // метод для ввода числа
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+long Faktor(int num)
+{
+    long res = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        res = res * i;
+    }
+    return res;
+}
+
+void PrintData(string msg, long value)
+{
+    Console.WriteLine(msg + value);
+}
+int numN = ReadData ("Введите число N: ");
+long OutResult = Faktor(numN);
+PrintData ("Факториал числа " + numN + " равен ", OutResult);
