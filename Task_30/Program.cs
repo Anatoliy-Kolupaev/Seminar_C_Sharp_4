12c2df788679d1b1ba5cbde0de81aeb7d095ca29
Anatoliy K <778966@mail.ru>
2022-09-26 23:59:36 +0500

    added 30 work

--- Task_30/Program.cs
+++ Task_30/Program.cs
@@ -4,3 +4,33 @@
 // случайном порядке.
 // Например: [1,0,1,1,0,1,0,0]
 
+int ReadData(string msg)
+{
+    System.Console.Write(msg);
+    int number = int.Parse(System.Console.ReadLine() ?? "0");
+    return number;
+}
+
+int[] Fill1DArr(int len, int lowBord, int highBord)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = new System.Random().Next(lowBord, highBord + 1);
+    }
+    return array;
+}
+
+void Print1DArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1] + "]");
+}
+
+int arrLen = ReadData("Введите длину массива: ");
+int[] arr = Fill1DArr(arrLen, 0, 1);
+Print1DArr(arr);
